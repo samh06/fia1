48c0bb023698f43fef8902a85746712a4e56d3d2
Sam Heinz <sam@samheinz.com>
2023-02-11 15:20:45 +1000

    creating files

--- UI/UI.py
+++ UI/UI.py
@@ -1,3 +1,11 @@
+import os 
+import shutil
 class UI:
-    def __init__():
+    def __init__(dir):
         print("UI")
+        
+        print(os.path.join(dir, "Data", "template"))
+        try:
+            shutil.copytree(os.path.join(dir, "Data", "template"), os.path.join(dir, "Data", "Sam"))
+        except FileExistsError:
+            print("Try again?")
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from UI import UI
-
-
+import os
+    
 def main():
-    UIs = UI.__init__()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    UI.__init__(dir_path)
 
 if __name__ == "__main__":
     main()
