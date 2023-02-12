9bdc7451c62b6ca9582754298e5761372b7795bf
Sam Heinz <sam@samheinz.com>
2023-02-12 16:03:20 +1000

    made some comments while i was waiting for my english video to finish adding captions

--- UI/UI.py
+++ UI/UI.py
@@ -4,8 +4,11 @@ from msvcrt import getch
 
 class UI:
     def __init__(self, dir):
+        # * Get banner from file
         f = open('banner', 'r')
         self.banner = f.read()
+        
+        # * Temporary directory copy test
         try:
             shutil.copytree(os.path.join(dir, "Data", "template"), os.path.join(dir, "Data", "Sam"))
         except FileExistsError:
@@ -14,26 +17,37 @@ class UI:
     def mainMenu(self):
         options = ["next", "back"]
         selection = 0
+        
+        # * Print options
         self.printOptions(self, options, selection)
+        
         while True:
+            # * Capture key input
             key = ord(getch())
-            # * Enter key
+            
+            # * Switch case for pressed keys
             match key:
+                # * Enter key
                 case 13:
                     break
+                # * Up arrow
                 case 72:
                     print("up")
                     selection -= 1
                     self.printOptions(self, options, selection)
+                # * Down arrow
                 case 80:
                     print("down")
                     selection += 1
                     self.printOptions(self, options, selection)
             
     def printOptions(self, options, selection):
+        # * Clear the terminal
         os.system("cls")
         print('\x1b[1;130;44m', self.banner, '\x1b[0m')
+        # * Print options on screen
         for option in range(len(options)):
+            # * Show indicator if current selected matches option
             if selection == option:
                 print('\x1b[6;30;42m' + "<•>", options[option], '\x1b[0m')
             else:
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from UI import UI
 import os
 from sys import platform, version_info
+
+# * Minimum Python version
 MIN_PYTHON = (3, 10)
 
 def main():
@@ -9,11 +11,17 @@ def main():
         exit("This program only runs on windows operatins sstems")
     if version_info < MIN_PYTHON:
         exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
-
+    
+    # * Get directory to pass to UI for copying files
     dir_path = os.path.dirname(os.path.realpath(__file__))
+    
+    # * Create interface and initialise it
     Interface = UI
     Interface.__init__(Interface, dir_path)
     Interface.mainMenu(Interface)
 
+# * Make sure main.py is the one being run
 if __name__ == "__main__":
     main()
+else:
+    print("Run main.py on its own.")
