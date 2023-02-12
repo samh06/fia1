94eefee6230dca8fad0aa3e071f2e71235a86f54
Sam Heinz <sam@samheinz.com>
2023-02-12 14:14:53 +1000

    Python minimum 3.10 and add switch case

--- UI/UI.py
+++ UI/UI.py
@@ -18,22 +18,17 @@ class UI:
         while True:
             key = ord(getch())
             # * Enter key
-            if key == 13:
-                break
-            elif key == 72:
-                print("up")
-                selection -= 1
-                self.printOptions(self, options, selection)
-            elif key == 80:
-                print("down")
-                selection += 1
-                self.printOptions(self, options, selection)
-            # elif key == 75:
-            #     print("left")
-            #     self.printOptions(self, options, selection)
-            # elif key == 77:
-            #     print("right")
-            #     self.printOptions(self, options, selection)
+            match key:
+                case 13:
+                    break
+                case 72:
+                    print("up")
+                    selection -= 1
+                    self.printOptions(self, options, selection)
+                case 80:
+                    print("down")
+                    selection += 1
+                    self.printOptions(self, options, selection)
             
     def printOptions(self, options, selection):
         os.system("cls")
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from UI import UI
 import os
-from sys import platform
-    
+from sys import platform, version_info
+MIN_PYTHON = (3, 10)
+
 def main():
+    # * Platform and Python Version checks.
     if platform != "win32":
-        print("This program only runs on windows operatins sstems")
-        exit()
+        exit("This program only runs on windows operatins sstems")
+    if version_info < MIN_PYTHON:
+        exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
+
     dir_path = os.path.dirname(os.path.realpath(__file__))
     Interface = UI
     Interface.__init__(Interface, dir_path)
